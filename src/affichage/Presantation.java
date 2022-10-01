4bc53ac14d375ba713a016e862bda103f7dd4fd8
abdessalm staili <abdoycode@gmail.com>
2022-10-01 20:26:37 +0100

    clas Presantation to affiche menu de view

--- src/affichage/Presantation.java
+++ src/affichage/Presantation.java
@@ -1,31 +1,45 @@
 package affichage;
+import affichage.view.Admin;
 
 import java.util.Scanner;
 
 public class Presantation {
 
-    public int MenuGlobal(){
+    public void Presantation(){
+        System.out.println("bonjour dans MCNSS :");
+      while (true){
+          int choixGlobal = this.MenuGlobal();
+          switch (choixGlobal){
+              case 1 :
+                  int admin = Admin.login();
+                  if(admin==0){
+                      continue;
+                  }else {
+                      int choix = Admin.MenuAdmin();
+                      if(choix==0){
+                          continue;
+                      }
 
-        while (true){
-            System.out.println("bonjour dans MCNSS :\nchoixi Votre Role");
+                  }
+              case 2 :
+
+              case 3 :
+
+              case 4 :
+                  System.exit(0);
+              default:
+                  System.out.println("\033[0;31mchoix invalid\033[0m");
+                  continue;
+          }
+      }
+
+    }
+    public int MenuGlobal(){
+            System.out.println("choixi Votre Role");
             System.out.println("1: Admin \n2: Agent\n3: Patient\n4: Exit");
             Scanner scanMenuGlobal = new Scanner(System.in);
             int choixMenuGlobal = scanMenuGlobal.nextInt();
-            if(choixMenuGlobal>4 || choixMenuGlobal<1){
-                System.out.println("\033[0;31mchoix invalid\033[0m");
-                continue;
-            }else if(choixMenuGlobal==4){
-                System.exit(0);
-            }else {
-                return choixMenuGlobal;
-            }
-        }
-
+            return choixMenuGlobal;
     }
-
-
-
-
-
 
 }
