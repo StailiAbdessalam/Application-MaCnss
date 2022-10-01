a18b1b35dd54baef9903fac75688fbf629fe7d79
abdessalm staili <abdoycode@gmail.com>
2022-10-01 20:25:56 +0100

    inial all the view in main global the project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import affichage.Presantation;
 import models.Admin;
 import java.util.Scanner;
 
@@ -5,6 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        Presantation affichage = new Presantation();
+        affichage.Presantation();
+
+
+
+        System.exit(0);
+
+
+
+
+
 
         System.out.println("hello word");
         System.out.print("Matricule : ");
