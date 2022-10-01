d8d87e0b97e04cc8ca7670fe891ee7ff5c7fb3b3
nemliProg <younesbuzz@gmail.com>
2022-10-02 00:46:59 +0100

    function to add agent created

--- src/models/Agent.java
+++ src/models/Agent.java
@@ -36,5 +36,29 @@ public class Agent extends Person {
         return null;
     }
 
+    public Boolean addAgent(){
+        boolean status = true;
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/macnss","root","");
+            String sql = "INSERT INTO agent (matricule,nom,prenom,email,password) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            connection.setAutoCommit(false);
+            ps.setString(1,this.matricule);
+            ps.setString(2,this.lastName);
+            ps.setString(3,this.firstName);
+            ps.setString(4,this.email);
+            ps.setString(5,this.passWord);
+            status = ps.execute();
+            connection.commit();
+            ps.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+
+
 
 }
