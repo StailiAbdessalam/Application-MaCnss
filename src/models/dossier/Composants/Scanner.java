8b4f2ee0b6d030c2408ba0011192aab50459160a
nemliProg <younesbuzz@gmail.com>
2022-10-02 03:36:26 +0100

    addScanner func created in Scanner class

--- src/models/dossier/Composants/Scanner.java
+++ src/models/dossier/Composants/Scanner.java
@@ -1,10 +1,36 @@
 package models.dossier.Composants;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Scanner {
         private String code;
-
-        public Scanner(String code) {
+        private String codeDossier;
+        public Scanner(String code, String codeDossier) {
             this.code = code;
+            this.codeDossier = codeDossier;
         }
+
+    public Boolean addScanner(){
+        boolean status = true;
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/macnss","root","");
+            String sql = "INSERT INTO scanner (code,code_dossier) VALUES (?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            connection.setAutoCommit(false);
+            ps.setString(1,this.code);
+            ps.setString(2,this.codeDossier);
+
+            status = ps.execute();
+            connection.commit();
+            ps.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return status;
+    }
 
 }
