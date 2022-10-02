02d84b298ca63f0634eac1ebdbcee61f4ee07c91
abdessalm staili <abdoycode@gmail.com>
2022-10-02 11:16:18 +0100

    add method to check cle the authantificaton

--- src/affichage/view/Agent.java
+++ src/affichage/view/Agent.java
@@ -1,17 +1,42 @@
 package affichage.view;
-
 import affichage.global.Globalmethod;
-
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Agent {
     public static int login() {
-        Boolean checklogin = Globalmethod.login();
-        if(checklogin){
-            return 1;
+        HashMap checklogin = Globalmethod.login("Email");
+            if(checklogin!=null){
+                    System.out.println("votre information et correct");
+                    String Cle = Globalmethod.sendEmail(checklogin.get("Email"));
+                   while (true){
+                       Boolean checkCle = checkCle(Cle);
+                       if(Boolean.TRUE.equals(checkCle)){
+                           return 1;
+                       }else if(checkCle==null){
+                           return 0;
+                       }else {
+                           System.out.println("votre cle incorrect");
+                           continue;
+                       }
+                   }
+            } else {
+                System.out.println("\033[0;31mvotre donnée et invalid\033[0m");
+                return 0;
+            }
+    }
+
+    public static Boolean checkCle(String cleEmail){
+        System.out.println("nous somme envoyer le cle d'entrer dans votre email || entrer votre cle d'entrer || or taper exit pour sortie :");
+        Scanner scanCle = new Scanner(System.in);
+        String Cle = scanCle.nextLine();
+        if(Objects.equals(Cle, cleEmail)){
+            return true;
+        }else if(Objects.equals(Cle, "exit")) {
+            return null;
         }else {
-            System.out.println("\033[0;31mvotre donnée et invalid\033[0m");
-            return 0;
+            return false;
         }
     }
     public static int MenuAgent(){
@@ -22,7 +47,6 @@ public class Agent {
             int choix = scanChoix.nextInt();
             switch (choix){
                 case 1:
-
                     continue;
                 case 2:
 
@@ -45,4 +69,5 @@ public class Agent {
         }
 
     }
+
 }
