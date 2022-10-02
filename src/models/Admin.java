0f3143dbf79712485b4fcdbd163099ef7fa2907e
nemliProg <younesbuzz@gmail.com>
2022-10-02 22:38:52 +0100

    getPassword func added to Admin Model class

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -19,7 +19,6 @@ public class Admin extends Person {
 			connection.setAutoCommit(false);
 			ps.setString(1,email);
 			ResultSet rs = ps.executeQuery();
-
 			if (rs.next() == false) {
 				return null;
 			} else {
@@ -28,6 +27,32 @@ public class Admin extends Person {
 					return true;
 				else
 					return false;
+			}
+
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/*
+	* getPassword take email and return password if email doesn't exist return null
+	* Parameter : String Email
+	* Return String (PassWord)
+	 */
+	public static String getPassword(String email){
+		try{
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/macnss","root","");
+			String sql = "select email,password from admin where email = ?";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			connection.setAutoCommit(false);
+			ps.setString(1,email);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next() == false) {
+				return null;
+			} else {
+				String pw = rs.getString(2);
+				return pw;
 			}
 
 		}catch (SQLException e){
