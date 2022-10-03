4927b66f7a5971b8bca1d75b73a28f29c2e9f9e3
nemliProg <younesbuzz@gmail.com>
2022-10-03 10:36:39 +0100

    authenticate method created in AgentController class

--- src/controllers/person/AgentController.java
+++ src/controllers/person/AgentController.java
@@ -1,4 +1,18 @@
 package controllers.person;
 
+import models.Admin;
+
 public class AgentController {
+    public Boolean authenticate(String email, String password) {
+        Admin admin = new Admin();
+        String result = admin.getPassword(email,"agent","email");
+        if (result == null) {
+            return null;
+        } else {
+            if (result.equals(password))
+                return true;
+            else
+                return false;
+        }
+    }
 }
