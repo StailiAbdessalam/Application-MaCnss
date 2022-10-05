432b8272bb5f5bc0a65f212f50ac39caa7f9f96c
nemliProg <younesbuzz@gmail.com>
2022-10-05 22:59:49 +0100

    some chenges

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import affichage.Presantation;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         Presantation affichage = new Presantation();
         affichage.Presantation();
         System.exit(0);
