e7a2e58ea1761120c3fbf5b7d4dd3b730a987c89
abdessalm staili <abdoycode@gmail.com>
2022-10-05 11:16:00 +0100

    in a system controller to add tretment in part dossier

---
+++ src/controllers/System/SystemController.java
@@ -0,0 +1,15 @@
+package controllers.System;
+
+import models.System.System;
+
+public class SystemController {
+    protected String CodeDossier;
+    public SystemController(String codeDossier) {
+        this.CodeDossier = codeDossier;
+    }
+
+    public void checkDossier(){
+        System systemModel=new System();
+        String[] dossier = systemModel.getDossier();
+    }
+}
