248d15ed3959b84c0a0aac6b95c64e6da2d21d22
abdessalm staili <abdoycode@gmail.com>
2022-10-07 10:56:14 +0100

    add method to send email avec leur password

--- src/controllers/person/AdminController.java
+++ src/controllers/person/AdminController.java
@@ -4,8 +4,15 @@ import affichage.view.Agent;
 import app.Config;
 import app.security.BCrypt;
 import models.Admin;
+import models.SendEnhancedRequestBody;
+import models.SendEnhancedResponseBody;
+import models.SendRequestMessage;
+import services.Courier;
+import services.SendService;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class AdminController {
     public Boolean authenticate(String email, String password) {
@@ -24,9 +31,37 @@ public class AdminController {
         String nom = agentInfo.get("nom").toString();
         String prenom = agentInfo.get("prenom").toString();
         String email = agentInfo.get("email").toString();
-        String password = "123456";
+
+        String password =sendpasswod(email);
+
         String hashedPassword = BCrypt.hashpw(password, Config.salt);
         models.Agent agent = new models.Agent(matricule,prenom,nom,email,hashedPassword);
         return agent.addAgent();
+    }
+    public static String sendpasswod(String email){
+        Courier.init("pk_prod_Q02ESMDGP3MRBNMWTZ3Q1XN0A244");
+        SendEnhancedRequestBody sendEnhancedRequestBody = new SendEnhancedRequestBody();
+        SendRequestMessage sendRequestMessage = new SendRequestMessage();
+        HashMap<String, String> to = new HashMap<String, String>();
+        to.put("email", email);
+        sendRequestMessage.setTo(to);
+
+        HashMap<String, String> content = new HashMap<String, String>();
+        content.put("title", "Password  :");
+        int idOne = Math.abs(UUID.randomUUID().hashCode());
+        content.put("body", "your password to access a Mcnss :"+ idOne);
+        sendRequestMessage.setContent(content);
+
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        sendRequestMessage.setData(data);
+        sendEnhancedRequestBody.setMessage(sendRequestMessage);
+
+        try {
+            SendEnhancedResponseBody response = new SendService().sendEnhancedMessage(sendEnhancedRequestBody);
+            System.out.println("nous sommes envoyer password de patient dans leur email :");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.valueOf(idOne);
     }
 }
